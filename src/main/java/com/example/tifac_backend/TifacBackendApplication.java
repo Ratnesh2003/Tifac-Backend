65f6fa7edf2158ce9676a27ea14c47b66130b3cd
Ratnesh Mishra <ratneshmishrarulz@gmail.com>
2023-04-24 19:21:39 +0530

    Initial commit

---
+++ src/main/java/com/example/tifac_backend/TifacBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tifac_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TifacBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TifacBackendApplication.class, args);
+    }
+
+}
